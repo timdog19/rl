f13962a9b24b445a89d622c3775752a1f5f8e728
timdog19 <102239203+timdog19@users.noreply.github.com>
2022-04-02 22:50:20 +0300

    Update and rename Roadmap.js to roadmapsection.js

--- src/components/roadmapsection.js
+++ src/components/roadmapsection.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import timelineElements from "./data/timelineElement.js";
 
 import {
@@ -10,55 +8,12 @@ import {
 
 import "react-vertical-timeline-component/style.min.css";
 
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { CSSRulePlugin } from "gsap/CSSRulePlugin";
 
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(CSSRulePlugin);
-
-function Roadmap() {
-    let titleelment = useRef(null)
-    let roadmapelement = useRef(null)
-    let triggerelement = useRef(null)
-
-    useEffect(()=> {
-        gsap.from(roadmapelement,{
-            scrollTrigger:{
-                trigger: triggerelement,
-                endTrigger: triggerelement,
-                toggleActions: "restart pause restart pause",
-                start: "35% 95%",
-                end: "100% 5%",
-                markers: false
-            },
-            duration: 2,
-            opacity: 0,
-            y: 100,
-            ease: "expo.out"
-        });
-        gsap.from(titleelment,{
-            scrollTrigger:{
-                trigger: triggerelement,
-                endTrigger: triggerelement,
-                toggleActions: "restart pause restart pause",
-                start: "35% 95%",
-                end: "100% 5%",
-                markers: false
-            },
-            duration: 2,
-            opacity: 0,
-            y: 100,
-            ease: "expo.out"
-        });
-    }, [])
-    
-
-class Roadmap extends React.Component {
+class RoadMapSection extends React.Component {
 
     render() {
         return (
-            <div className="w-full flex flex-col place-items-center" id="roadmap">
+            <div className="w-full flex flex-col place-items-center" id="roadmapSection">
                 <div className="w-84.5% pb-36 flex flex-col place-items-center space-y-36">
                     <div className="w-max">
                         <h1 className="text-white text-5xl font-extrabold italic">THE ROADMAP</h1>
@@ -96,4 +51,4 @@ class Roadmap extends React.Component {
     }
 }
 
-export default Roadmap
+export default RoadMapSection
